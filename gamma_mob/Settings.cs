69bd7b7253e169c1a85baa1618785f7ce85b8e7c
Alexandr Pavlov <a.pavlov@sgbi.ru>
2021-12-03 10:47:44 +0300

    Добавлены новые настройки в файле settings.xml: * Datalogic Desktop Utility, * Datalogic Buttons, * Barcode scanner, * Backlight. Настройки применяются при запуске программы, если были изменения в ключевых настройках DDU, то ТСД перезагружается для применения новых настроек.

--- gamma_mob/Settings.cs
+++ gamma_mob/Settings.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Xml;
 using gamma_mob.Common;
 using System;
+using Microsoft.Win32;
+using Datalogic.API;
 
 namespace gamma_mob
 {
@@ -29,20 +31,20 @@ namespace gamma_mob
             var xdoc = new XmlDocument();
             xdoc.Load(m_settingsPath);
             XmlElement root = xdoc.DocumentElement;
-            for (var j = 0; j < root.ChildNodes.Count; j++)
+            // Add settings to the NameValueCollection.
+            m_settings = new NameValueCollection();
+            if (root.ChildNodes.Count == 1)
             {
-                XmlNode node = root.ChildNodes.Item(j);
+                XmlNode node = root.ChildNodes.Item(0);
                 XmlNodeList nodeList = node.ChildNodes;
-
-                // Add settings to the NameValueCollection.
-                m_settings = new NameValueCollection();
                 for (var i = 0; i < nodeList.Count; i++)
                 {
-                    m_settings.Add(node.Name +"."+ nodeList.Item(i).Attributes["key"].Value, nodeList.Item(i).Attributes["value"].Value);
+                    m_settings.Add("progSettings.Gamma."+nodeList.Item(i).Attributes["key"].Value, nodeList.Item(i).Attributes["value"].Value);
                 }
-                if ((m_settings.Get("appSettings.SecondServerIP") ?? String.Empty) == String.Empty)
-                    m_settings.Add("appSettings.SecondServerIP", "");
-                if (ServerIP.Substring(0, 3) != "192")
+                if ((m_settings.Get("progSettings.Gamma.SecondServerIP") ?? String.Empty) == String.Empty)
+                    m_settings.Add("progSettings.Gamma.SecondServerIP", "");
+                
+                if (ServerIP != null && ServerIP.Substring(0, 3) != "192")
                 {
                     if (SecondServerIP == "")
                         SecondServerIP = ServerIP;
@@ -56,51 +58,70 @@ namespace gamma_mob
                         Shared.SaveToLog("Error Create(m_currentSecondServerFlag)");
                     }
                 }
+
             }
-            m_settings.Add("appSettings.CurrentServer", File.Exists(m_currentSecondServerFlag)
-                 ? m_settings.Get("appSettings.SecondServerIP") : m_settings.Get("appSettings.ServerIP"));
+            else
+            {
+                for (var h = 0; h < root.ChildNodes.Count; h++)
+                {
+                    XmlNode nodeSettings = root.ChildNodes.Item(h);
+                    XmlNodeList nodeSettingsList = nodeSettings.ChildNodes;
+                    for (var j = 0; j < nodeSettingsList.Count; j++)
+                    {
+                        XmlNode node = nodeSettings.ChildNodes.Item(j);
+                        XmlNodeList nodeList = node.ChildNodes;
+                        for (var i = 0; i < nodeList.Count; i++)
+                        {
+                            m_settings.Add(nodeSettings.Name + "." + node.Name + "." + nodeList.Item(i).Attributes["key"].Value, nodeList.Item(i).Attributes["value"].Value);
+                        }
+                    }
+                }
+                UpdateDeviceSettings();
+            }
+            m_settings.Add("progSettings.Gamma.CurrentServer", File.Exists(m_currentSecondServerFlag)
+                 ? m_settings.Get("progSettings.Gamma.SecondServerIP") : m_settings.Get("progSettings.Gamma.ServerIP"));
             
         }
 
         public static string ServerIP
         {
-            get { return m_settings.Get("appSettings.ServerIP"); }
-            set { m_settings.Set("appSettings.ServerIP", value); }
+            get { return m_settings.Get("progSettings.Gamma.ServerIP"); }
+            set { m_settings.Set("progSettings.Gamma.ServerIP", value); }
         }
 
         public static string UserName
         {
-            get { return m_settings.Get("appSettings.UserName"); }
-            set { m_settings.Set("appSettings.UserName", value); }
+            get { return m_settings.Get("progSettings.Gamma.UserName"); }
+            set { m_settings.Set("progSettings.Gamma.UserName", value); }
         }
 
         public static string Password
         {
-            get { return m_settings.Get("appSettings.Password"); }
-            set { m_settings.Set("appSettings.Password", value); }
+            get { return m_settings.Get("progSettings.Gamma.Password"); }
+            set { m_settings.Set("progSettings.Gamma.Password", value); }
         }
 
         public static string Database
         {
-            get { return m_settings.Get("appSettings.Database"); }
-            set { m_settings.Set("appSettings.Database", value); }
+            get { return m_settings.Get("progSettings.Gamma.Database"); }
+            set { m_settings.Set("progSettings.Gamma.Database", value); }
         }
 
         public static string TimeOut
         {
-            get { return m_settings.Get("appSettings.TimeOut"); }
-            set { m_settings.Set("appSettings.TimeOut", value); }
+            get { return m_settings.Get("progSettings.Gamma.TimeOut"); }
+            set { m_settings.Set("progSettings.Gamma.TimeOut", value); }
         }
 
         public static string SecondServerIP
         {
-            get { return m_settings.Get("appSettings.SecondServerIP"); }
-            set { m_settings.Set("appSettings.SecondServerIP", value); }
+            get { return m_settings.Get("progSettings.Gamma.SecondServerIP"); }
+            set { m_settings.Set("progSettings.Gamma.SecondServerIP", value); }
         }
 
         public static string CurrentServer
         {
-            get { return m_settings.Get("appSettings.CurrentServer"); }
+            get { return m_settings.Get("progSettings.Gamma.CurrentServer"); }
             //set { m_settings.Set("CurrentServer", value); }
         }
 
@@ -109,7 +130,7 @@ namespace gamma_mob
             var tw = new XmlTextWriter(m_settingsPath, Encoding.UTF8);
             tw.WriteStartDocument();
             tw.WriteStartElement("configuration");
-            tw.WriteStartElement("appSettings");
+            tw.WriteStartElement("progSettings");
 
             for (int i = 0; i < m_settings.Count; ++i)
             {
@@ -136,7 +157,7 @@ namespace gamma_mob
             try
             {
                 if (File.Exists(m_currentSecondServerFlag)) File.Delete(m_currentSecondServerFlag);
-                m_settings.Set("appSettings.CurrentServer", ServerIP);
+                m_settings.Set("progSettings.Gamma.CurrentServer", ServerIP);
                 ret = true;
             }
             catch
@@ -154,7 +175,7 @@ namespace gamma_mob
                 try
                 {
                     File.CreateText(m_currentSecondServerFlag);
-                    m_settings.Set("appSettings.CurrentServer", SecondServerIP);
+                    m_settings.Set("progSettings.Gamma.CurrentServer", SecondServerIP);
                     ret = true;
                 }
                 catch
@@ -163,6 +184,178 @@ namespace gamma_mob
                 }
             }
             return ret;
+        }
+        
+        private static void UpdateDeviceSettings()
+        {
+            bool reboot = false;
+            foreach (var item in m_settings.Keys)
+            {
+                if (item.ToString().Substring(0, 11) == "dduSettings" || item.ToString().Substring(0, 11) == "dlbSettings" || item.ToString().Substring(0, 11) == "backlightSe")
+                    try
+                    {
+                        var key = item.ToString().Replace("dduSettings.", "").Replace("dlbSettings.", "").Replace("backlightSettings.", "");
+                        var l = key.IndexOf(".");
+                        var keyName = key.Substring(0, l).Replace("__", "\\");
+                        var valueName = key.Substring(l + 1, key.Length - l - 1);
+                        var val = m_settings.Get(item.ToString());
+                        var regVal = Registry.GetValue(keyName, valueName, "not exist");
+                        if (((item.ToString() == @"dduSettings.HKEY_CURRENT_USER__Software__Datalogic__DDU__GENERAL.DduLoad")
+                            || (item.ToString() == @"dduSettings.HKEY_CURRENT_USER__Software__Datalogic__DDU__GENERAL.Password")
+                            || (item.ToString() == @"dduSettings.HKEY_CURRENT_USER__Software__Datalogic__DDU__UserInterface.EnableStartMenu"))
+                            && val != regVal.ToString())
+                            reboot = true;
+                        
+                        if (regVal == null || val != regVal.ToString())
+                            Registry.SetValue(keyName, valueName, val, valueName == "Password" || val == "" || !IsInt(val) ? RegistryValueKind.String : RegistryValueKind.DWord);
+                    }
+                    catch (Exception ex)
+                    {
+                        Shared.SaveToLog("Error Update registry ("+item+")");
+                    }
+                else
+                    if (item.ToString().Substring(0, 11) == "scanSetting")
+                    {
+                        try
+                        {
+                            int val = int.Parse(m_settings.Get(item.ToString()));
+                            int par = -1;
+                            switch (item.ToString())
+                            {
+                                case "scanSettings.EAN13.ENABLE":
+                                    par = Param.EAN13_ENABLE;
+                                    break;
+                                case "scanSettings.EAN13.TO_ISBN":
+                                    par = Param.EAN13_TO_ISBN;
+                                    break;
+                                case "scanSettings.EAN13.TO_ISSN":
+                                    par = Param.EAN13_TO_ISSN;
+                                    break;
+                                case "scanSettings.EAN13.SEND_CHECK":
+                                    par = Param.EAN13_SEND_CHECK;
+                                    break;
+                                case "scanSettings.EAN13.SEND_SYS":
+                                    par = Param.EAN13_SEND_SYS;
+                                    break;
+
+                                case "scanSettings.I25.ENABLE":
+                                    par = Param.I25_ENABLE;
+                                    break;
+                                case "scanSettings.I25.MIN_LENGTH":
+                                    par = Param.I25_MIN_LENGTH;
+                                    break;
+                                case "scanSettings.I25.MAX_LENGTH":
+                                    par = Param.I25_MAX_LENGTH;
+                                    break;
+                                case "scanSettings.I25.ENABLE_CHECK":
+                                    par = Param.I25_ENABLE_CHECK;
+                                    break;
+                                case "scanSettings.I25.SEND_CHECK":
+                                    par = Param.I25_SEND_CHECK;
+                                    break;
+                                case "scanSettings.I25.CASE_CODE":
+                                    par = Param.I25_CASE_CODE;
+                                    break;
+
+                                case "scanSettings.CODE128.ENABLE":
+                                    par = Param.CODE128_ENABLE;
+                                    break;
+                                case "scanSettings.CODE128.MIN_LENGTH":
+                                    par = Param.CODE128_MIN_LENGTH;
+                                    break;
+                                case "scanSettings.CODE128.MAX_LENGTH":
+                                    par = Param.CODE128_MAX_LENGTH;
+                                    break;
+                                case "scanSettings.CODE128.ENABLE_GS1_128":
+                                    par = Param.CODE128_ENABLE_GS1_128;
+                                    break;
+
+                                case "scanSettings.CODE39.ENABLE":
+                                    par = Param.CODE39_ENABLE;
+                                    break;
+                                case "scanSettings.CODE39.MIN_LENGTH":
+                                    par = Param.CODE39_MIN_LENGTH;
+                                    break;
+                                case "scanSettings.CODE39.MAX_LENGTH":
+                                    par = Param.CODE39_MAX_LENGTH;
+                                    break;
+                                case "scanSettings.CODE39.ENABLE_CHECK":
+                                    par = Param.CODE39_ENABLE_CHECK;
+                                    break;
+                                case "scanSettings.CODE39.SEND_CHECK":
+                                    par = Param.CODE39_SEND_CHECK;
+                                    break;
+                                case "scanSettings.CODE39.FULL_ASCII":
+                                    par = Param.CODE39_FULL_ASCII;
+                                    break;
+
+                                case "scanSettings.CODEBAR.ENABLE":
+                                    par = Param.CODE39_ENABLE;
+                                    break;
+                                case "scanSettings.CODE93.ENABLE":
+                                    par = Param.CODE39_ENABLE;
+                                    break;
+                                case "scanSettings.EAN8.ENABLE":
+                                    par = Param.EAN8_ENABLE;
+                                    break;
+                                case "scanSettings.GS1_14.ENABLE":
+                                    par = Param.GS1_14_ENABLE;
+                                    break;
+                                case "scanSettings.GS1_EXP.ENABLE":
+                                    par = Param.GS1_EXP_ENABLE;
+                                    break;
+                                case "scanSettings.GS1_LIMIT.ENABLE":
+                                    par = Param.GS1_LIMIT_ENABLE;
+                                    break;
+                                case "scanSettings.MSI.ENABLE":
+                                    par = Param.MSI_ENABLE;
+                                    break;
+                                case "scanSettings.PHARMA39.ENABLE":
+                                    par = Param.PHARMA39_ENABLE;
+                                    break;
+                                case "scanSettings.S25.ENABLE":
+                                    par = Param.S25_ENABLE;
+                                    break;
+                                case "scanSettings.TRIOPTIC.ENABLE":
+                                    par = Param.TRIOPTIC_ENABLE;
+                                    break;
+                                case "scanSettings.UPCA.ENABLE":
+                                    par = Param.UPCA_ENABLE;
+                                    break;
+                                case "scanSettings.UPCE0.ENABLE":
+                                    par = Param.UPCE0_ENABLE;
+                                    break;
+                            }
+                            var curVal = Decode.GetParam(par);
+                            if (curVal != val)
+                                Decode.SetParam(par, val);
+                        }
+                        catch
+                        {
+                            Shared.SaveToLog("Error Update scaner parameter (" + item + ")");
+                        }
+                    }
+
+            }
+            if (reboot)
+            {
+                Shared.SaveToLog("Error - Rebooting Device");
+                Device.Reset(Device.BootType.Warm);
+            }
+        }
+
+        private static bool IsInt(string s)
+        {
+            bool isInt = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!char.IsDigit(s[i]))
+                {
+                    isInt = false;
+                    break;
+                }
+            }
+            return isInt;
         }
     }
 }
